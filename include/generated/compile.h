ac7aa8ce40f77ebba720c6102f4c67982d3b4eb6
Marc Alexander <admin@m-a-styles.de>
2011-09-21 14:13:23 +0200

    Fixed boot issues

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,9 +1,9 @@
-/* This file is auto generated, version 20 */
+/* This file is auto generated, version 7 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#20 PREEMPT Tue Sep 20 01:24:20 PDT 2011"
-#define LINUX_COMPILE_TIME "01:24:20"
+#define UTS_VERSION "#7 PREEMPT Wed Sep 21 13:49:22 CEST 2011"
+#define LINUX_COMPILE_TIME "13:49:22"
 #define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "VBOX-ACA"
+#define LINUX_COMPILE_HOST "marc-G71GX"
 #define LINUX_COMPILE_DOMAIN "(none)"
 #define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
