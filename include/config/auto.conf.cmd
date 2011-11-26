5c1ba04c48fe97ce30304b5ec0afe826bfa8217f
Marc Alexander <admin@m-a-styles.de>
2011-11-26 14:09:42 +0100

    Patched to 2.6.35.14

--- include/config/auto.conf.cmd
+++ include/config/auto.conf.cmd
@@ -621,7 +621,7 @@ deps_config := \
 include/config/auto.conf: \
 	$(deps_config)
 
-ifneq "$(KERNELVERSION)" "2.6.35.12"
+ifneq "$(KERNELVERSION)" "2.6.35.14"
 include/config/auto.conf: FORCE
 endif
 ifneq "$(ARCH)" "arm"
