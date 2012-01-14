107dac003194bd045c912a7ec1b87a9a168340bb
Marc Alexander <admin@m-a-styles.de>
2012-01-15 00:57:31 +0100

    Fix no sound bug when using bluetooth Source: http://review.cyanogenmod.com/#change,9758
    Original commit message:
    qdsp6: Allow audio_rx_path_id to be zero, fixes Bluetooth
    
    In the refactoring done by commit 8913be80, the new adie_rx_path_enable
    added a check for audio_rx_path_id!=0 that didn't exist before. It
    becomes 0 for example when using Bluetooth voice dial. I found that
    CAF's kernels also do this, but they make the same check around every
    adie_proceed_to_stage() call throughout. Since we only have it in one
    place, the audio can get into a confused state and stop working.
    
    This patch removes the audio_rx_path_id check, so the logic more closely
    matches what we had before, and now BT is fine again.
    
    Change-Id: I9a4f437597b1a5f509517b88f8fd9369e6f7ad7d

--- arch/arm/mach-msm/qdsp6_1550/q6audio.c
+++ arch/arm/mach-msm/qdsp6_1550/q6audio.c
@@ -1599,17 +1599,15 @@ static int audio_update_acdb(uint32_t adev, uint32_t acdb_id)
 #ifdef CONFIG_QSD_AUDIO_CALLREC
 static void adie_rx_path_enable(uint32_t acdb_id)
 {
-    AUDIO_INFO("%s\n", __func__);
-	if (audio_rx_path_id) {
-		adie_enable();
-		adie_set_path(adie, audio_rx_path_id, ADIE_PATH_RX);
-		adie_set_path_freq_plan(adie, ADIE_PATH_RX, 48000);
+	AUDIO_INFO("%s\n", __func__);
+	adie_enable();
+	adie_set_path(adie, audio_rx_path_id, ADIE_PATH_RX);
+	adie_set_path_freq_plan(adie, ADIE_PATH_RX, 48000);
 
-		adie_proceed_to_stage(adie, ADIE_PATH_RX,
-			ADIE_STAGE_DIGITAL_READY);
-		adie_proceed_to_stage(adie, ADIE_PATH_RX,
-			ADIE_STAGE_DIGITAL_ANALOG_READY);
-	}
+	adie_proceed_to_stage(adie, ADIE_PATH_RX,
+		ADIE_STAGE_DIGITAL_READY);
+	adie_proceed_to_stage(adie, ADIE_PATH_RX,
+		ADIE_STAGE_DIGITAL_ANALOG_READY);
 }
 
 static void q6_rx_path_enable(int reconf, uint32_t acdb_id)
