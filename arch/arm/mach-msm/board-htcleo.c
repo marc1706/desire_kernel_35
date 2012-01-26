870e35a941dbc4fdd49957b57bf52fad8f23f804
Marc Alexander <admin@m-a-styles.de>
2012-01-27 00:49:04 +0100

    Reduce expected power collapse kHz to 128000

--- arch/arm/mach-msm/board-htcleo.c
+++ arch/arm/mach-msm/board-htcleo.c
@@ -975,8 +975,8 @@ static struct msm_acpu_clock_platform_data htcleo_clock_data = {
 	.acpu_switch_time_us	= 20,
 	.max_speed_delta_khz	= 256000,
 	.vdd_switch_time_us	= 62,
-	.power_collapse_khz	= 245000,
-	.wait_for_irq_khz	= 245000, // @todo: bravo sets this to 0, try it out
+	.power_collapse_khz	= 128000,
+	.wait_for_irq_khz	= 128000,
 //	.wait_for_irq_khz	= 19200,   // TCXO
 };
 
