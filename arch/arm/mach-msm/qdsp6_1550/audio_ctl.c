c1c10edaa06617fe300871a05b157b5819e963b2
Marc Alexander <admin@m-a-styles.de>
2012-02-06 22:51:00 +0100

    [ARM] msm8k: fixes for audio_ctl and pcm_out
    -- q6_ioctl was not doing proper checks on user data
    -- pcm_ioctl would crash if AUDIO_SET_VOLUME got called before AUDIO_START
    
    Signed-off-by: Iliyan Malchev <malchev@google.com>

--- arch/arm/mach-msm/qdsp6_1550/audio_ctl.c
+++ arch/arm/mach-msm/qdsp6_1550/audio_ctl.c
@@ -146,28 +146,40 @@ static long q6_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	case AUDIO_SWITCH_DEVICE:
 		rc = copy_from_user(&id, (void *)arg, sizeof(id));
 		AUDIO_INFO("SWITCH DEVICE %d, acdb %d\n", id[0], id[1]);
-		if (!rc)
+		if (rc) {
+			pr_err("%s: bad user address\n", __func__);
+			rc = -EFAULT;
+		} else
 			rc = q6audio_do_routing(id[0], id[1]);
 		break;
 	case AUDIO_SET_VOLUME:
 		rc = copy_from_user(&n, (void *)arg, sizeof(n));
-		if (!rc)
+		if (rc) {
+			pr_err("%s: bad user address\n", __func__);
+			rc = -EFAULT;
+		} else
 			rc = q6audio_set_rx_volume(n);
 		break;
 	case AUDIO_SET_MUTE:
 		rc = copy_from_user(&n, (void *)arg, sizeof(n));
-		if (!rc)
+		if (rc) {
+			pr_err("%s: bad user address\n", __func__);
+			rc = -EFAULT;
+		} else
 			rc = q6audio_set_tx_mute(n);
 		break;
 	case AUDIO_UPDATE_ACDB:
 		rc = copy_from_user(&id, (void *)arg, sizeof(id));
-		if (!rc)
+		if (rc) {
+			pr_err("%s: bad user address\n", __func__);
+			rc = -EFAULT;
+		} else
 			rc = q6audio_update_acdb(id[0], id[1]);
 		break;
 	case AUDIO_START_VOICE:
-		if (arg == 0) {
+		if (arg == 0)
 			id[0] = id[1] = 0;
-		} else if (copy_from_user(&id, (void*) arg, sizeof(id))) {
+		else if (copy_from_user(&id, (void*) arg, sizeof(id))) {
 			pr_info("voice: copy acdb_id from user failed\n");
 			rc = -EFAULT;
 			break;
@@ -189,7 +201,10 @@ static long q6_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 		break;
 	case AUDIO_REINIT_ACDB:
 		rc = copy_from_user(&filename, (void *)arg, sizeof(filename));
-		if (!rc)
+		if (rc) {
+			pr_err("%s: bad user address\n", __func__);
+			rc = -EFAULT;
+		} else
 			rc = q6audio_reinit_acdb(filename);
 		break;
 	case AUDIO_ENABLE_AUXPGA_LOOPBACK: {
