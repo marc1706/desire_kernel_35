2de327dd769651e48a4675d02d8cbbbbe1c0be70
Marc Alexander <admin@m-a-styles.de>
2012-05-30 19:18:36 +0200

    msm: qdsp6: venc: add missing IOCTL command

--- include/linux/msm_q6vdec.h
+++ include/linux/msm_q6vdec.h
@@ -46,6 +46,8 @@
 #define VDEC_IOCTL_GETDECATTRIBUTES   _IOR(VDEC_IOCTL_MAGIC, 10, \
 						struct vdec_dec_attributes)
 #define VDEC_IOCTL_GETVERSION   _IOR(VDEC_IOCTL_MAGIC, 11, struct vdec_version)
+#define VDEC_IOCTL_PERFORMANCE_CHANGE_REQ  _IOW(VDEC_IOCTL_MAGIC, 14, \
+						unsigned int)
 
 enum {
 	VDEC_FRAME_DECODE_OK,
@@ -82,6 +84,13 @@ enum {
 	VDEC_COLOR_FORMAT_NV21 = 0x01,
 	VDEC_COLOR_FORMAT_NV21_YAMOTO = 0x02
    };
+
+enum {
+	PERF_REQUEST_SET_MIN = 0,
+	PERF_REQUEST_LOWER,
+	PERF_REQUEST_RAISE,
+	PERF_REQUEST_SET_MAX
+};
 
 struct vdec_input_buf_info {
 	u32 offset;
@@ -167,7 +176,7 @@ struct vdec_frame_info {
 	u32 concealed_macblk_num;	/* number of concealed macro blk */
 	u32 flags;		/* input flags */
 	u32 performance_stats;	/* performance statistics returned by decoder */
-	u32 data3;		/* user data field 3 */
+	u32 data3;              /* user data field 3 */
 };
 
 struct vdec_buf_info {
