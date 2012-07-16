6e9be917d365efc942342cd89325b537a2db3d64
marc1706 <admin@m-a-styles.de>
2012-07-16 15:41:43 +0200

    cpufreq: fix incorrect tabbing

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -742,82 +742,82 @@ static int should_io_be_busy(void)
 
 static void dbs_refresh_callback(struct work_struct *unused)
 {
-       struct cpufreq_policy *policy;
-       struct cpu_dbs_info_s *this_dbs_info;
-       
-       if (lock_policy_rwsem_write(0) < 0)
-               return;
+	struct cpufreq_policy *policy;
+	struct cpu_dbs_info_s *this_dbs_info;
 
-       this_dbs_info = &per_cpu(od_cpu_dbs_info, 0);
-       policy = this_dbs_info->cur_policy;
+	if (lock_policy_rwsem_write(0) < 0)
+		return;
 
-       if (policy->cur < policy->max) {
-               policy->cur = policy->max;
+	this_dbs_info = &per_cpu(od_cpu_dbs_info, 0);
+	policy = this_dbs_info->cur_policy;
 
-               __cpufreq_driver_target(policy, policy->max,
-                                       CPUFREQ_RELATION_L);
-               this_dbs_info->prev_cpu_idle = get_cpu_idle_time(0,
-                               &this_dbs_info->prev_cpu_wall);
-       }
-       unlock_policy_rwsem_write(0);
+	if (policy->cur < policy->max) {
+		policy->cur = policy->max;
+
+		__cpufreq_driver_target(policy, policy->max,
+					CPUFREQ_RELATION_L);
+		this_dbs_info->prev_cpu_idle = get_cpu_idle_time(0,
+				&this_dbs_info->prev_cpu_wall);
+	}
+	unlock_policy_rwsem_write(0);
 }
 
 static DECLARE_WORK(dbs_refresh_work, dbs_refresh_callback);
 
 static void dbs_input_event(struct input_handle *handle, unsigned int type,
-               unsigned int code, int value)
+		unsigned int code, int value)
 {
-       schedule_work_on(0, &dbs_refresh_work);
+	schedule_work_on(0, &dbs_refresh_work);
 }
 
 static int dbs_input_connect(struct input_handler *handler,
-               struct input_dev *dev, const struct input_device_id *id)
+		struct input_dev *dev, const struct input_device_id *id)
 {
-       struct input_handle *handle;
-       int error;
+	struct input_handle *handle;
+	int error;
 
-       handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
-       if (!handle)
-               return -ENOMEM;
+	handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
+	if (!handle)
+		return -ENOMEM;
 
-       handle->dev = dev;
-       handle->handler = handler;
-       handle->name = "cpufreq";
+	handle->dev = dev;
+	handle->handler = handler;
+	handle->name = "cpufreq";
 
-       error = input_register_handle(handle);
-       if (error)
-               goto err2;
+	error = input_register_handle(handle);
+	if (error)
+		goto err2;
 
-       error = input_open_device(handle);
-       if (error)
-               goto err1;
+	error = input_open_device(handle);
+	if (error)
+		goto err1;
 
-       return 0;
+	return 0;
 err1:
-       input_unregister_handle(handle);
+	input_unregister_handle(handle);
 err2:
-       kfree(handle);
-       return error;
+	kfree(handle);
+	return error;
 }
 
 static void dbs_input_disconnect(struct input_handle *handle)
 {
-       input_close_device(handle);
-       input_unregister_handle(handle);
-       kfree(handle);
+	input_close_device(handle);
+	input_unregister_handle(handle);
+	kfree(handle);
 }
 
 static const struct input_device_id dbs_ids[] = {
-       { .driver_info = 1 },
-       { },
+	{ .driver_info = 1 },
+	{ },
 };
 
 static struct input_handler dbs_input_handler = {
-       .event          = dbs_input_event,
-       .connect        = dbs_input_connect,
-       .disconnect     = dbs_input_disconnect,
-       .name           = "cpufreq_ond",
-       .id_table       = dbs_ids,
+	.event		= dbs_input_event,
+	.connect	= dbs_input_connect,
+	.disconnect	= dbs_input_disconnect,
+	.name		= "cpufreq_ond",
+	.id_table	= dbs_ids,
 };
 
 static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
@@ -886,7 +886,7 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 			dbs_tuners_ins.io_is_busy = should_io_be_busy();
 		}
 		if (!cpu)
-                       rc = input_register_handler(&dbs_input_handler);
+			rc = input_register_handler(&dbs_input_handler);
 		mutex_unlock(&dbs_mutex);
 
 		mutex_init(&this_dbs_info->timer_mutex);
@@ -901,7 +901,7 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 		mutex_destroy(&this_dbs_info->timer_mutex);
 		dbs_enable--;
 		if (!cpu)
-                       input_unregister_handler(&dbs_input_handler);
+			input_unregister_handler(&dbs_input_handler);
 		mutex_unlock(&dbs_mutex);
 		if (!dbs_enable)
 			sysfs_remove_group(cpufreq_global_kobject,
