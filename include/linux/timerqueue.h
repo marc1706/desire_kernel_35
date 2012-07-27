45fd407ecd90924163f8fe64fe2a697453d8da55
marc1706 <admin@m-a-styles.de>
2012-07-27 15:26:45 +0200

    timers: Add rb_init_node() to allow for stack allocated rb nodes
    In cases where a timerqueue_node or some structure that utilizes
    a timerqueue_node is allocated on the stack, gcc would give warnings
    caused by the timerqueue_init()'s calling RB_CLEAR_NODE, which
    self-references the nodes uninitialized data.
    
    The solution is to create an rb_init_node() function that zeros
    the rb_node structure out and then calls RB_CLEAR_NODE(), and
    then call the new init function from timerqueue_init().
    
    CC: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- include/linux/timerqueue.h
+++ include/linux/timerqueue.h
@@ -26,7 +26,7 @@ extern struct timerqueue_node *timerqueue_iterate_next(
 
 static inline void timerqueue_init(struct timerqueue_node *node)
 {
-	RB_CLEAR_NODE(&node->node);
+	rb_init_node(&node->node);
 }
 
 static inline void timerqueue_init_head(struct timerqueue_head *head)
