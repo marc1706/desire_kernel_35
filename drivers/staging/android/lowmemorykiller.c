f2e47dce2030e23f6c7c8f7c64b3bd0336e58010
marc1706 <admin@m-a-styles.de>
2012-12-16 13:28:46 +0100

    staging: android: lowmemkiller: merge aosp version of lowmemkiller
    Revision: f665be65

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -34,9 +34,8 @@
 #include <linux/mm.h>
 #include <linux/oom.h>
 #include <linux/sched.h>
+#include <linux/profile.h>
 #include <linux/notifier.h>
-
-#define DEBUG_LEVEL_DEATHPENDING 6
 
 static uint32_t lowmem_debug_level = 2;
 static int lowmem_adj[6] = {
@@ -54,26 +53,12 @@ static size_t lowmem_minfree[6] = {
 };
 static int lowmem_minfree_size = 4;
 
-static size_t lowmem_minfile[6] = {
-	1536,
-	2048,
-	4096,
-	5120,
-	5632,
-	6144
-};
-static int lowmem_minfile_size = 6;
-
 static struct task_struct *lowmem_deathpending;
-static unsigned long lowmem_deathpending_timeout;
-static uint32_t lowmem_check_filepages = 0;
 
 #define lowmem_print(level, x...)			\
 	do {						\
-		if (lowmem_debug_level >= (level)) {	\
-			printk("lowmem: ");		\
+		if (lowmem_debug_level >= (level))	\
 			printk(x);			\
-		}					\
 	} while (0)
 
 static int
@@ -87,53 +72,14 @@ static int
 task_notify_func(struct notifier_block *self, unsigned long val, void *data)
 {
 	struct task_struct *task = data;
-
 	if (task == lowmem_deathpending) {
 		lowmem_deathpending = NULL;
-		lowmem_print(2, "deathpending end %d (%s)\n",
-			task->pid, task->comm);
+		task_handoff_unregister(&task_nb);
 	}
-
 	return NOTIFY_OK;
 }
 
-static void dump_deathpending(struct task_struct *t_deathpending)
-{
-	struct task_struct *p;
-
-	if (lowmem_debug_level < DEBUG_LEVEL_DEATHPENDING)
-		return;
-
-	BUG_ON(!t_deathpending);
-	lowmem_print(DEBUG_LEVEL_DEATHPENDING, "deathpending %d (%s)\n",
-		t_deathpending->pid, t_deathpending->comm);
-
-	read_lock(&tasklist_lock);
-	for_each_process(p) {
-		struct mm_struct *mm;
-		struct signal_struct *sig;
-		int oom_adj;
-		int tasksize;
-
-		task_lock(p);
-		mm = p->mm;
-		sig = p->signal;
-		if (!mm || !sig) {
-			task_unlock(p);
-			continue;
-		}
-		oom_adj = sig->oom_adj;
-		tasksize = get_mm_rss(mm);
-		task_unlock(p);
-		lowmem_print(DEBUG_LEVEL_DEATHPENDING,
-			"  %d (%s), adj %d, size %d\n",
-			p->pid, p->comm,
-			oom_adj, tasksize);
-	}
-	read_unlock(&tasklist_lock);
-}
-
-static int lowmem_shrink(struct shrinker *s, int nr_to_scan, gfp_t gfp_mask)
+static int lowmem_shrink(int nr_to_scan, gfp_t gfp_mask)
 {
 	struct task_struct *p;
 	struct task_struct *selected = NULL;
@@ -145,10 +91,7 @@ static int lowmem_shrink(struct shrinker *s, int nr_to_scan, gfp_t gfp_mask)
 	int selected_oom_adj;
 	int array_size = ARRAY_SIZE(lowmem_adj);
 	int other_free = global_page_state(NR_FREE_PAGES);
-	int other_file = global_page_state(NR_FILE_PAGES) -
-						global_page_state(NR_SHMEM);
-	int lru_file = global_page_state(NR_ACTIVE_FILE) +
-			global_page_state(NR_INACTIVE_FILE);
+	int other_file = global_page_state(NR_FILE_PAGES);
 
 	/*
 	 * If we already have a death outstanding, then
@@ -156,26 +99,21 @@ static int lowmem_shrink(struct shrinker *s, int nr_to_scan, gfp_t gfp_mask)
 	 * that we have nothing further to offer on
 	 * this pass.
 	 *
+	 * Note: Currently you need CONFIG_PROFILING
+	 * for this to work correctly.
 	 */
-	if (lowmem_deathpending &&
-	    time_before_eq(jiffies, lowmem_deathpending_timeout)) {
-		dump_deathpending(lowmem_deathpending);
+	if (lowmem_deathpending)
 		return 0;
-	}
 
 	if (lowmem_adj_size < array_size)
 		array_size = lowmem_adj_size;
 	if (lowmem_minfree_size < array_size)
 		array_size = lowmem_minfree_size;
 	for (i = 0; i < array_size; i++) {
-		if (other_free < lowmem_minfree[i]) {
-			if (other_file < lowmem_minfree[i] ||
-				(lowmem_check_filepages &&
-				(lru_file < lowmem_minfile[i]))) {
-
-				min_adj = lowmem_adj[i];
-				break;
-			}
+		if (other_free < lowmem_minfree[i] &&
+		    other_file < lowmem_minfree[i]) {
+			min_adj = lowmem_adj[i];
+			break;
 		}
 	}
 	if (nr_to_scan > 0)
@@ -229,11 +167,24 @@ static int lowmem_shrink(struct shrinker *s, int nr_to_scan, gfp_t gfp_mask)
 			     p->pid, p->comm, oom_adj, tasksize);
 	}
 	if (selected) {
+		if (fatal_signal_pending(selected)) {
+			pr_warning("process %d is suffering a slow death\n",
+				   selected->pid);
+			read_unlock(&tasklist_lock);
+			return rem;
+		}
 		lowmem_print(1, "send sigkill to %d (%s), adj %d, size %d\n",
 			     selected->pid, selected->comm,
 			     selected_oom_adj, selected_tasksize);
+		/*
+		 * If CONFIG_PROFILING is off, then task_handoff_register()
+		 * is a nop. In that case we don't want to stall the killer
+		 * by setting lowmem_deathpending.
+		 */
+#ifdef CONFIG_PROFILING
 		lowmem_deathpending = selected;
-		lowmem_deathpending_timeout = jiffies + HZ;
+		task_handoff_register(&task_nb);
+#endif
 		force_sig(SIGKILL, selected);
 		rem -= selected_tasksize;
 	}
@@ -250,7 +201,6 @@ static struct shrinker lowmem_shrinker = {
 
 static int __init lowmem_init(void)
 {
-	task_free_register(&task_nb);
 	register_shrinker(&lowmem_shrinker);
 	return 0;
 }
@@ -258,7 +208,6 @@ static int __init lowmem_init(void)
 static void __exit lowmem_exit(void)
 {
 	unregister_shrinker(&lowmem_shrinker);
-	task_free_unregister(&task_nb);
 }
 
 module_param_named(cost, lowmem_shrinker.seeks, int, S_IRUGO | S_IWUSR);
@@ -267,11 +216,6 @@ module_param_array_named(adj, lowmem_adj, int, &lowmem_adj_size,
 module_param_array_named(minfree, lowmem_minfree, uint, &lowmem_minfree_size,
 			 S_IRUGO | S_IWUSR);
 module_param_named(debug_level, lowmem_debug_level, uint, S_IRUGO | S_IWUSR);
-
-module_param_named(check_filepages , lowmem_check_filepages, uint,
-		   S_IRUGO | S_IWUSR);
-module_param_array_named(minfile, lowmem_minfile, uint, &lowmem_minfile_size,
-			 S_IRUGO | S_IWUSR);
 
 module_init(lowmem_init);
 module_exit(lowmem_exit);
